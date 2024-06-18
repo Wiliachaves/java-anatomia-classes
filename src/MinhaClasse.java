15eaebceee693402e8763fdd9a412606de95f7bd
Wiliachaves <wiliachaves2020@gmail.com>
2024-06-18 17:35:11 -0300

    resultado-nomecompleto

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,9 +1,15 @@
 public class MinhaClasse {
 
 public static void main (String[] args ) {
-    System.out.print ("Ola,tudo bom ?");
+    String primeiroNome ="Wilia";
+    String segundoNome = "Chaves";
 
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+}
 
+public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
 }
 
 }
